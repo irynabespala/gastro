2d9a7271343fb0d3c94bb87044aa0e8041fb27b8
irynabespala <bespalairyna246@student.voskh.cz>
2026-06-13 13:08:57 +0200

    oprava chyb

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 // Data pro jídla
 export const surovaJidla = [
-    { id: 1, nazev: "Chačapuri Adjaruli", zakladniCena: 189, hmotnost: 400, cenaKrabicky: 15, img: "img/chačapuri.jpg" },
+    { id: 1, nazev: "Chačapuri Adjaruli", zakladniCena: 189, hmotnost: 400, cenaKrabicky: 15, img: "img/chacapuri.jpg" },
     { id: 2, nazev: "Chinkali s hovězím masem", zakladniCena: 175, hmotnost: 350, cenaKrabicky: 15, img: "img/chinkali.jpg" },
-    { id: 3, nazev: "Šašlik z vepřové krkovičky", zakladniCena: 220, hmotnost: 300, cenaKrabicky: 20, img: "img/šašlik.jpg" },
+    { id: 3, nazev: "Šašlik z vepřové krkovičky", zakladniCena: 220, hmotnost: 300, cenaKrabicky: 20, img: "img/saslik.jpg" },
     { id: 4, nazev: "Dort Napoleon", zakladniCena: 95, hmotnost: 180, cenaKrabicky: 10, img: "img/napoleon.jpg" },
     { id: 5, nazev: "Lula Kebab", zakladniCena: 185, hmotnost: 250, cenaKrabicky: 15, img: "img/lula.jpg" }
 ];
@@ -11,5 +11,5 @@ export const surovaNapoje = [
     { id: 6, nazev: "Gruzínské víno Saperavi", zakladniCena: 350, objem: 0.75, vratnaZaloha: 0, img: "img/vino.jpg" },
     { id: 7, nazev: "Limonáda Natakhtari (Tarhun)", zakladniCena: 65, objem: 0.5, vratnaZaloha: 5, img: "img/tarhun.jpg" },
     { id: 8, nazev: "Minerální voda Borjomi", zakladniCena: 55, objem: 0.5, vratnaZaloha: 5, img: "img/borjomi.jpg" },
-    { id: 9, nazev: "Přírodní mošt z granátového jablka", zakladniCena: 79, objem: 0.33, vratnaZaloha: 0, img: "img/mošt.jpg" }
+    { id: 9, nazev: "Přírodní mošt z granátového jablka", zakladniCena: 79, objem: 0.33, vratnaZaloha: 0, img: "img/most.jpg" }
 ];
